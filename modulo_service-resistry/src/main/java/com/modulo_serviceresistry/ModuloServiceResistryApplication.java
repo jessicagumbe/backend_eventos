27134c197cb0ca08a3f4ed159ea8200e386a4477
jessicagumbe <65067449+jessicagumbe@users.noreply.github.com>
2022-10-19 10:52:00 +0100

    criação do projecto

---
+++ modulo_service-resistry/src/main/java/com/modulo_serviceresistry/ModuloServiceResistryApplication.java
@@ -0,0 +1,13 @@
+package com.modulo_serviceresistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloServiceResistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloServiceResistryApplication.class, args);
+    }
+
+}
---
+++ modulo_service-resistry/src/test/java/com/modulo_serviceresistry/ModuloServiceResistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulo_serviceresistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloServiceResistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
